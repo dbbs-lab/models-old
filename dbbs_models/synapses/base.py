d742df155254074958832d03143b22de2be3866f
Robin De Schepper <robin.deschepper93@gmail.com>
2019-12-03 17:45:08 +0100

    changed order of kwargs to better fit use.

--- dbbs_models/synapses/base.py
+++ dbbs_models/synapses/base.py
@@ -2,11 +2,11 @@ import glia as g
 
 class Synapse:
 
-    def __init__(self, cell, section, point_process_name, variant=None, attributes = {}):
+    def __init__(self, cell, section, point_process_name, attributes = {}, variant=None):
         self._cell = cell
         self._section = section
         self._point_process_name = point_process_name
-        self._point_process_glia_name = g.resolve_process(point_process_name, pkg="dbbs_mod_collection", variant=variant)
+        self._point_process_glia_name = g.resolve(point_process_name, pkg="dbbs_mod_collection", variant=variant)
         self._point_process = g.process(section, point_process_name, pkg="dbbs_mod_collection", variant=variant)
         for key, value in attributes.items():
             setattr(self._point_process, key, value)
