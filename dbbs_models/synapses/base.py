76e419824a013b0df4c4af0ba0dab22361e95cc9
Robin De Schepper <robin.deschepper93@gmail.com>
2019-12-06 16:16:07 +0100

    made a synapse a NEURON-able type.

--- dbbs_models/synapses/base.py
+++ dbbs_models/synapses/base.py
@@ -7,6 +7,9 @@ class Synapse:
         self._section = section
         self._point_process_name = point_process_name
         self._point_process_glia_name = g.resolve(point_process_name, pkg="dbbs_mod_collection", variant=variant)
-        self._point_process = g.process(section, point_process_name, pkg="dbbs_mod_collection", variant=variant)
+        self._point_process = g.insert(section, point_process_name, pkg="dbbs_mod_collection", variant=variant)
         for key, value in attributes.items():
             setattr(self._point_process, key, value)
+
+    def __neuron__(self):
+        return self._point_process
