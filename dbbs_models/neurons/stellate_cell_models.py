db6f00b5205a3b53443cefe0fcb6a4eca4a2bb08
martina <martinafrances.rizza01@universitadipavia.it>
2020-01-23 14:51:40 +0100

    completed stellate model

--- dbbs_models/neurons/stellate_cell_models.py
+++ dbbs_models/neurons/stellate_cell_models.py
@@ -46,73 +46,72 @@ class StellateCell(NeuronModel):
         "dendrites": {
             "mechanisms": [], "attributes": {}
         },
-        "basal_dendrites": {
-            "synapses": ['AMPA'],
-            "mechanisms": ['Leak','Nav1_6','Kv1_1','Kca1_1','Kca2_2','Cav2_2','cdp5'],
+        "proximal_dendrites": {
+            "synapses": ['AMPA', 'NMDA'],
+            "mechanisms": ['Leak', 'Cav3_2', 'Cav3_3', 'Kv1_1','Kv4_3','Kca1_1','Kca2_2','Cav2_1','cdp5'],
             "attributes": {
-                "Ra": 122, "cm": 3, "ena": 60, "ek": -80, "eca": 137,
-                ("e", "Leak"): -59,
-                ("gmax", "Leak"): 0.00003,
-                ("gbar", "Nav1_6"): 0.00596389701963,
-                ("gbar", "Kv1_1"): 0.00300075070483,
-                ("gbar", "Kca1_1"): 0.01992292703235,
-                ("gkbar", "Kca2_2"): 0.01067448459892,
-                ("gcanbar", "Cav2_2"): 0.00098761689603,
-                ("TotalPump", "cdp5"): 0.7e-9,
+                "Ra": 110, "cm": 1.5, "ek": -84, "eca": 137.5,
+                ("e", "Leak"): -48,
+                ("gmax", "Leak"): 0.000008,
+                ("gcabar", "Cav3_2"): 0.00070661092763,
+                ("pcabar", "Cav3_3"): 0.00001526216781,
+                ("gbar", "Kv1_1"): 0.00906810561650,
+                ("gkbar", "Kv4_3"): 0.00264204713540,
+                ("gbar", "Kca1_1"): 0.00499205404769,
+                ("gkbar", "Kca2_2"): 0.00000326194117,
+                ("pcabar", "Cav2_1"): 0.0005,
+                ("TotalPump", "cdp5"): 1e-9,
             }
         },
-        "apical_dendrites": {
-            "synapses": ['AMPA'],
-            "mechanisms": ['Leak', 'Nav1_6', 'Kv1_1', 'Kca1_1', 'Kca2_2', 'Cav2_3', 'Cav3_1', 'cdp5'],
+        "distal_dendrites": {
+            "synapses": ['AMPA', 'NMDA'],
+            "mechanisms": ['Leak', 'Kv1_1', 'Kca1_1', 'Kca2_2', 'Cav2_1', 'cdp5'],
             "attributes":  {
-                "Ra": 122, "cm": 3, "ena": 60, "ek": -80, "eca": 137,
-                ("e", "Leak"): -59,
-                ("gmax", "Leak"): 0.00003,
-                ("gbar", "Nav1_6"): 0.0032004946448,
-                ("gbar", "Kv1_1"): 0.00366791747215,
-                ("gbar", "Kca1_1"): 0.0207352478961,
-                ("gkbar", "Kca2_2"): 0.00597385039361,
-                ("gcabar", "Cav2_3"): 7.181872437E-05,
-                ("pcabar", "Cav3_1"): 7.03521203E-06,
-                ("TotalPump", "cdp5"): 2e-9,
+                "Ra": 110, "cm": 1.5, "ek": -84, "eca": 137.5,
+                ("e", "Leak"): -48,
+                ("gmax", "Leak"): 0.000008,
+                ("gbar", "Kv1_1"): 0.00237825442906,
+                ("gbar", "Kca1_1"): 0.00226329455766,
+                ("gkbar", "Kca2_2"): 0.00001079984416,
+                ("pcabar", "Cav2_1"): 0.00025,
+                ("TotalPump", "cdp5"): 1e-9,
             }
         },
         "axon": {
-            "mechanisms": ['Leak', 'Kv1_1', 'Nav1_6', 'Kv3_4', 'cdp5'],
+            "mechanisms": ['Leak', 'Kv1_1', 'Nav1_6', 'Kv3_4', 'HCN1' 'cdp5'],
             "attributes": {
-                "Ra": 122, "cm": 1, "ena": 60, "ek": -80, "eca": 137,
-                ("e", "Leak"): -59,
-                ("gmax", "Leak"): 0.00001,
-                ("gbar", "Kv1_1"): 0.004,
-                ("gbar", "Nav1_6"): 9.670147548E-05,
-                ("gkbar", "Kv3_4"): 0.00633480317392,
-                ("TotalPump", "cdp5"): 1e-9,
+                "Ra": 110, "cm": 1, "ena": 60, "ek": -84, "eh": -34,
+                ("e", "Leak"): -48,
+                ("gmax", "Leak"): 0.000008,
+                ("gbar", "Kv1_1"): 0.00271359229578,
+                ("gbar", "Nav1_6"): 0.00835931586458,
+                ("gkbar", "Kv3_4"): 0.01153520393521,
+                ("gbar", "HCN1"): 0.00070017344082,
             }
         },
         "axon_initial_segment": {
-            "mechanisms": ['Leak', 'HCN1', 'HCN2', 'Nav1_6', 'Kv3_4', 'Km', 'cdp5'],
+            "mechanisms": ['Leak', 'HCN1', 'Nav1_6', 'Kv1_1', 'Kv3_4', 'Km', 'cdp5'],
             "attributes": {
-                "Ra": 122, "cm": 1, "ena": 60, "ek": -80, "eca": 137,
-                ("e", "Leak"): -59,
-                ("gmax", "Leak"): 0.00003,
-                ("gbar", "HCN1"): 0.00020635332351,
-                ("gbar", "HCN2"): 0.0002816526031,
-                ("gbar", "Nav1_6"): 0.39006484091705,
-                ("gkbar", "Kv3_4"): 0.29455503229075,
-                ("gkbar", "Km"): 0.00030327654923,
-                ("TotalPump", "cdp5"): 1e-9,
+                "Ra": 110, "cm": 1, "ena": 60, "ek": -84, "eh": -34,
+                ("e", "Leak"): -48,
+                ("gmax", "Leak"): 0.000008,
+                ("gbar", "HCN1"): 0.00099184971498,
+                ("gbar", "Nav1_6"): 0.4,
+                ("gbar", "Kv1_1"): 0.00492841685426,
+                ("gkbar", "Kv3_4"):0.03351450571128,
+                ("gkbar", "Km"):0.00007960307413,
             }
         }
     }
 
     labels = {
-        "basal_dendrites": {
+        "proximal_dendrites": {
             "from": "dendrites",
-            "id": lambda id: id >= 0 and id <= 3 or id >= 16 and id <= 17 or id >= 33 and id <= 41 or id == 84 or id >= 105 and id <= 150
+            "radius": lambda r: r >= 0.3
         },
-        "apical_dendrites": {
+        "distal_dendrites": {
             "from": "dendrites",
-            "id": lambda id: id >= 4 and id <= 15 or id >= 18 and id <= 32 or id >= 42 and id <= 83 or id >= 85 and id <= 104
+            "radius": lambda r: r < 0.3
         },
         "axon_initial_segment": {
             "from": "axon",
