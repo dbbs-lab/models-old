50aaf999af50cff71f2a96dc77333dfd90399edb
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-27 23:25:50 +0100

    added purkinje cell to `neurons` namespace

--- dbbs_models/neurons/__init__.py
+++ dbbs_models/neurons/__init__.py
@@ -1,4 +1,5 @@
 from .granule_cell_models import GranuleCell
 from .stellate_cell_models import StellateCell
 from .golgi_cell_models import GolgiCell
+from .purkinje_models import PurkinjeCell
 from .base import NeuronModel, Section
