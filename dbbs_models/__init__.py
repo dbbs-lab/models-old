10ac48bc05e9be9e5fb409ee18d4a7bc902abfbb
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-28 01:02:43 +0100

    moved obsolete neurons directory

--- dbbs_models/__init__.py
+++ dbbs_models/__init__.py
@@ -1,1 +1,5 @@
+from .granule_cell_models import GranuleCell
+from .stellate_cell_models import StellateCell
+from .golgi_cell_models import GolgiCell
+
 __version__ = "0.2.0"
--- dbbs_models/neurons/__init__.py
+++
@@ -1,3 +0,0 @@
-from .granule_cell_models import GranuleCell
-from .stellate_cell_models import StellateCell
-from .golgi_cell_models import GolgiCell
