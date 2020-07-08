d8ca0b62c691a8ed03d9af1cee6bd1885fe5da5a
Robin De Schepper <robin.deschepper93@gmail.com>
2020-07-08 20:37:36 +0200

    fixed test typo

--- tests/test_models.py
+++ tests/test_models.py
@@ -11,11 +11,11 @@ class TestGranule(unittest.TestCase):
 class TestPurkinje(unittest.TestCase):
 
     def test_autorhythm(self):
-        results = run_protocol("PurkinjeCell", "autorhythm", duration=300)
+        results = run_protocol("PurkinjeCell", "autorhythm")
         self.assertEqual(results.Spikecount[0], 3, "Incorrect spike count.")
 
 class TestBasket(unittest.TestCase):
 
     def test_autorhythm(self):
-        results = run_protocol("BasketCell", "autorhythm")
-        self.assertEqual(results.Spikecount[0], 3, "Incorrect spike count.")
+        results = run_protocol("BasketCell", "autorhythm", duration=300)
+        self.assertEqual(results.Spikecount[0], 6, "Incorrect spike count.")
